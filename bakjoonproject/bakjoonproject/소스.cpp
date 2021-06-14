c467affcd4cffe1caeca28e137894719fd567596
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-06-14 20:52:26 +0900

    project
    commit

---
+++ bakjoonproject/bakjoonproject/소스.cpp
@@ -0,0 +1,30 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+int main(void)
+{
+	int testcase=0;
+	int student=0;
+	int** score = (int**)malloc(sizeof(int*) * testcase);
+	scanf_s("%d", &testcase);
+	for (int i = 0; i < testcase; i++)
+	{
+		score[i] = (int*)malloc(sizeof(int) * student);
+		scanf_s("%d", &student);
+		for (int j = 0; j < student; j++)
+		{
+			scanf_s("%d", &score[i][j]);
+		}
+
+	}
+
+
+
+
+	
+
+
+
+
+}*/
