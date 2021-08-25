08b9fd81030ab89800cb889e441122e8fea34124
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2021-08-26 00:53:57 +0900

    algo
    dd

---
+++ function/function/function/No15596.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+
+
+long long sum(std::vector<int>& a)
+{
+	int num;
+	int n;
+	long long ans = 0;
+	std::cout << "how many :";
+	std::cin >> n;
+	std::cout << std::endl;
+	for (int i = 0;i < n;i++)
+	{
+		std::cout << "input num:";
+		std::cin >> num;
+		std::cout << std::endl;
+		a.push_back(num);
+	}
+	for (int i = 0;i < n;i++)
+	{
+		ans = ans+a[i];
+	}
+
+	return ans;
+	
+}
+
+/*
+#include <vector>
+using namespace std;
+long long sum(vector<int>& a) {
+	long long ans = 0;
+	for (int i =0;i<a.length();i++)
+		ans += *iter;
+	return ans;
+}
+*/
